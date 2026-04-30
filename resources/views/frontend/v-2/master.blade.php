e6cbb7ca5d088e860fc03c9f4aef6cfda6a63d0c
Md Arman Ali <ceowebcareit@gmail.com>
2026-04-30 15:46:57 +0600

    add domain verification code

--- resources/views/frontend/v-2/master.blade.php
+++ resources/views/frontend/v-2/master.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') | {{env('APP_NAME')}}</title>
     <link rel="icon" href="{{asset('setting/'.$setting->logo)}}"/>
-    <meta name="facebook-domain-verification" content="yowovq4nz231o35tawk1l8syb3alz9" />
-    
+    <meta name="facebook-domain-verification" content="bbd9cy7yqu3qnmr9ycalz6dou1eg2d" />
     <!-- Pavicon ICon -->
     @include('frontend.v-2.includes.style')
     <!-- Dynamic Theme Colors -->
